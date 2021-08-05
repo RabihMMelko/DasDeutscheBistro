f92ae12869ad799912b6e555177ee92ffa1d771c
Rabih Melko <rabih.melko@isae.edu.lb>
2021-08-05 23:34:03 +0300

    Gestion Panier Dynamique: Suppression Produit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,7 +15,7 @@ const hasItems = cartCtx.items.length > 0;
 
 console.log(cartCtx.items);
 const cartItemRemoveHandler = id => {
-  
+  cartCtx.removeItem(id)
 }  
 
 const cartItemAddHandler = (item) => {
